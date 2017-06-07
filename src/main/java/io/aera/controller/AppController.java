cfde5e27e4916351d748be4825d181376d29d65e
Azon <azon.sk@gmail.com>
2017-06-07 22:09:11 +0300

    add messages

--- src/main/java/io/aera/controller/AppController.java
+++ src/main/java/io/aera/controller/AppController.java
@@ -2,10 +2,16 @@ package io.aera.controller;
 
 public class AppController {
     public static void main(String[] args) {
+        System.out.println("Hello Vorobei");
+        System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello Jack");
+        System.out.println("Hello Jack");
+        System.out.println("Hello Vorobei");
         System.out.println("Hello Vorobei");
         System.out.println("Hello Cat");
         System.out.println("Hello Dog");
+        System.out.println("Hello Dog");
+        System.out.println("Hello Vorobei");
     }
 }
