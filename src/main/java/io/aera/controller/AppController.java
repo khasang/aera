3a5ff8da8493c353ebc51faea746be8eedf5b200
Azon <azon.sk@gmail.com>
2017-06-07 22:06:39 +0300

    delete jack and add two world

--- src/main/java/io/aera/controller/AppController.java
+++ src/main/java/io/aera/controller/AppController.java
@@ -2,7 +2,8 @@ package io.aera.controller;
 
 public class AppController {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Jack");
+        System.out.println("Hello Jack");
         System.out.println("Hello Jack");
         System.out.println("Hello Vorobei");
     }
