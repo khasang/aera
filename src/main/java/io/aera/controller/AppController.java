2fb3ebc2beb798262398a127bd7eafce4146630b
Azon <azon.sk@gmail.com>
2017-06-07 22:08:41 +0300

    add cat dog

--- src/main/java/io/aera/controller/AppController.java
+++ src/main/java/io/aera/controller/AppController.java
@@ -2,11 +2,8 @@ package io.aera.controller;
 
 public class AppController {
     public static void main(String[] args) {
-        System.out.println("Hello Jack");
-        System.out.println("Hello Jack");
-        System.out.println("Hello Jack");
-        System.out.println("Hello Vorobei");
-        System.out.println("Hello Cat");
-        System.out.println("Hello Dog");
+
+        System.out.println("Cat");
+        System.out.println("Dog");
     }
 }
