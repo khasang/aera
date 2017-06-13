61194d46995d3fa9fefc30ba32c63001baef9255
YauheniSavitski <07ersh@gmail.com>
2017-06-13 03:13:30 +0300

    add hometasks 1 and 2

--- src/main/java/io/aera/controller/AppController.java
+++ src/main/java/io/aera/controller/AppController.java
@@ -1,6 +1,7 @@
 package io.aera.controller;
 
 import io.aera.model.Cat;
+import io.aera.model.Dog;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,10 +12,19 @@ public class AppController {
     @Autowired
     private Cat cat;
 
+    @Autowired
+    private Dog dog;
+
     // localhost:8080/
     @RequestMapping("/")
     public String hello(Model model){
         model.addAttribute("attr", cat.getName());
         return "index";
+    }
+
+    @RequestMapping("/create")
+    public String createDogTable(Model model){
+        model.addAttribute("status", dog.createDogTable());
+        return "dog";
     }
 }
---
+++ web/WEB-INF/views/dog.jsp
@@ -0,0 +1,8 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Dog TableTitle</title>
+</head>
+<h1>${status}</h1>
+</body>
+</html>
