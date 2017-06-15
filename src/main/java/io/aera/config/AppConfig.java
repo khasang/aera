bc4b661d8ffb997ebeb01bed2b5dbdf9e3e51641
Viktoria <bvika@mail.ru>
2017-06-15 18:08:49 +0300

    add basic proj structure

--- src/main/java/io/aera/config/AppConfig.java
+++ src/main/java/io/aera/config/AppConfig.java
@@ -4,12 +4,10 @@ import io.aera.model.Cat;
 import io.aera.model.impl.CatImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
     @Bean
-    @Scope("singleton")
     public Cat cat() {
         return new CatImpl("Riska");
     }
--- src/main/java/io/aera/controller/AppController.java
+++ src/main/java/io/aera/controller/AppController.java
@@ -6,8 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.xml.ws.RequestWrapper;
-
 @Controller
 public class AppController {
     @Autowired
