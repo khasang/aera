ec199ddb717503ec40ae67c191949a9b69985308
Viktoria <bvika@mail.ru>
2017-06-15 20:07:16 +0300

    add class Message in proj

---
+++ src/main/java/io/aera/model/Message.java
@@ -0,0 +1,7 @@
+package io.aera.model;
+
+/**
+ * Created by Vika on 15.06.2017.
+ */
+public interface Message {
+}
---
+++ src/main/java/io/aera/model/impl/MessageImpl.java
@@ -0,0 +1,7 @@
+package io.aera.model.impl;
+
+/**
+ * Created by Vika on 15.06.2017.
+ */
+public class MessageImpl {
+}
