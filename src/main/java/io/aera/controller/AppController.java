93d85e30e7263461fff47c7c4e1769e45bb7a86a
Viktoria <bvika@mail.ru>
2017-06-22 22:38:39 +0300

    add Authentication from db

--- src/main/java/io/aera/controller/AppController.java
+++ src/main/java/io/aera/controller/AppController.java
@@ -4,9 +4,13 @@ import io.aera.model.Cat;
 import io.aera.model.Dog;
 import io.aera.model.Message;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AppController {
@@ -40,5 +44,14 @@ public class AppController {
     @RequestMapping("/admin/page")
     public String getAdmin() {
         return "admin";
+    }
+
+    // localhost:8080/password/admin
+    @RequestMapping(value = {"password/{password}"}, method = RequestMethod.GET)
+    public ModelAndView passwordEncode(@PathVariable("password") String password) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("password");
+        modelAndView.addObject("crypt", new BCryptPasswordEncoder().encode(password));
+        return modelAndView;
     }
 }
