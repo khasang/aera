e87ccddb620904d57fbbde534a5482e36a5f92be
Indyukov Sergey <SergeyI88>
2017-07-03 23:06:52 +0300

    create character and interface typecharacter

---
+++ src/main/java/io/aera/entity/interfaces/TypeCharacter.java
@@ -0,0 +1,14 @@
+package io.aera.entity.interfaces;
+
+/**
+ * Created by admin on 03.07.2017.
+ */
+public interface TypeCharacter {
+        public void go();
+        public void fight();
+        public void sleep();
+        public void eat();
+        public void heal();
+        public void death();
+
+}
