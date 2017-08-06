a483732b4e9ac2a0f1c1f9d79e99633981d50bcf
Sergey <WaRDeRMobile@gmail.com>
2017-08-06 14:45:47 +0300

    Add control by personage data on index page

--- src/main/java/io/aera/controller/AppController.java
+++ src/main/java/io/aera/controller/AppController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping
 public class AppController {
     @RequestMapping("/")
-    public String typePersonage() {
-        return "typePersonage";
+    public String personageControl() {
+        return "personageControl";
     }
 }
