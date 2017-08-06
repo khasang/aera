eb5878f7ac2d6740ff8f6dd327fb49bb620b2ed5
Sergey <WaRDeRMobile@gmail.com>
2017-08-06 16:04:52 +0300

    Clean code AppController

--- src/main/java/io/aera/controller/AppController.java
+++ src/main/java/io/aera/controller/AppController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping
 public class AppController {
-    @RequestMapping("/")
+    @RequestMapping("")
     public String personageControl() {
         return "personageControl";
     }
